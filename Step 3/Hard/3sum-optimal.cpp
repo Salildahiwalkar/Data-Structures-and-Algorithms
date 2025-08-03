5e9be9d218e56c48f13862490023c1ad94edaeb9
Salildahiwalkar <salildahiwalkar@gmail.com>
2025-08-03 12:54:10 +0530

    Create 3sum-optimal.cpp

---
+++ Step 3/Hard/3sum-optimal.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        int j = i + 1;
+        int k = n - 1;
+
+        while (j < k)
+        {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum > 0)
+            {
+                k--;
+            }
+            else if (sum < 0)
+            {
+                j++;
+            }
+            else
+            {
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
+                while (j < k && nums[k] == nums[k + 1])
+                    k--;
+            }
+        }
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    vector<vector<int>> ans = threeSum(arr);
+
+    for (auto it : ans)
+    {
+        for (auto i : it)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
