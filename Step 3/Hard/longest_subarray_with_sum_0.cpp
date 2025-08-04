dd79e9666dd11168f5da4dd5f2b9dbac602086c7
Salildahiwalkar <salildahiwalkar@gmail.com>
2025-08-04 13:05:15 +0530

    Create longest_subarray_with_sum_0.cpp

---
+++ Step 3/Hard/longest_subarray_with_sum_0.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LongestSubsetWithZeroSum(vector<int> arr)
+{
+    unordered_map<int, int> mpp;
+    int n = arr.size();
+    int sum = 0, maxi = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        if (sum == 0)
+        {
+            maxi = i + 1;
+        }
+        else
+        {
+            if (mpp.find(sum) != mpp.end())
+            {
+                maxi = max(maxi, i - mpp[sum]);
+            }
+            else
+            {
+                mpp[sum] = i;
+            }
+        }
+    }
+    return maxi;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    cout << LongestSubsetWithZeroSum(arr);
+}
